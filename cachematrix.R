03bb6c00b3b4ba10544f2c5defaf150a69860dee
walnerp <walnerp@hotmail.com>
2014-05-25 11:02:14 -0300

    Include de comment in functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,10 @@
+## This is a programming assignmente 2 
+## Define 2 functions (makeCacheMatrix and cacheSolve )
+
+
+##The makeCacheMatrix function recives the matrix and cache the variable type matrix()
+## and ## Return a matrix that is the inverse of 'mta'
+
 makeCacheMatrix <- function(mt = matrix()){   
   mtx <- mt
   
@@ -9,14 +16,12 @@ makeCacheMatrix <- function(mt = matrix()){
   return(mtx)
 }
 
-## Return a matrix that is the inverse of 'mta'
+## the cacheSolve funcition inverse of matrix, if not exist, If the original matrix and actual are identical
+## return de inverse matrix in cache
 
-
-  
 cacheSolve <- function(mta = matrix() ) {
     mt <-mta
-    ## if the inverse of matrix not exist and the original matrix and actual are identical
-    ## return de inverse matrix in cache
+    
     
     if(!is.na(mtxSC) && identical(mt,mtxac)) {
       
